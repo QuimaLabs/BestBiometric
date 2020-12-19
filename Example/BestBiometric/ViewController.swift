ab5559219e084005a462940b51fbbf0bffab923d
Raul Quispe <raul.quispe@globant.com>
2020-12-19 17:09:33 -0500

    Initial commit

---
+++ Example/BestBiometric/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BestBiometric
+//
+//  Created by aquima on 12/19/2020.
+//  Copyright (c) 2020 aquima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
